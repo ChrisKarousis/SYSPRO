62c9d107b6de16e895810078100343f0f8f31df8
ChrisKarousis <78919212+ChrisKarousis@users.noreply.github.com>
2023-07-15 16:15:04 +0300

    Delete ΧρήστοςΚαρούσηςProject1 directory

--- ΧρήστοςΚαρούσηςProject1/history.h
+++
@@ -1,9 +0,0 @@
-#ifndef HISTORY
-#define HISTORY
-
-#define MAX_HISTORY_COUNT 20
-
-#endif 
-
-void add_to_history(char** history, char* command, int *count);
-void print_history(char** history, int count);
